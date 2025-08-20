361d9f8c2cf8c8db69cabce5319818096e66463f
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-08-19 22:42:32 -0300

    Creation of get-all-projects use case

--- src/domain/use-cases/projects/projects.module.ts
+++ src/domain/use-cases/projects/projects.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GetAllProjectsService } from './get-all-projects.service';
+import { GetProjectByIdService } from './get-project-by-id.service';
 
 @Module({
-  providers: [GetAllProjectsService]
+  providers: [GetAllProjectsService, GetProjectByIdService]
 })
 export class ProjectsModule {}
