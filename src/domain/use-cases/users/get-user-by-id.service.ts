c4e5649b62f589fc802b2d6438ee0f0f02060bfa
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-08-26 21:04:21 -0300

    Created use case to get user by id

--- src/domain/use-cases/users/get-user-by-id.service.ts
+++ src/domain/use-cases/users/get-user-by-id.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from 'src/domain/interfaces/user.interface';
+import { UsersRepositoryService } from 'src/infrastructure/database/repositories/users.repository.service';
 
 @Injectable()
-export class GetUserByIdService {}
+export class GetUserByIdService {
+  constructor(private readonly usersRepository: UsersRepositoryService) {}
+
+  async execute(userId: number): Promise<IUser> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new Error('User não encontrado');
+    }
+
+    return user;
+  }
+}
