d41bd5abca6f64935793a6292bb2ca8eaee7dbac
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-08-26 21:42:22 -0300

    Created use case to retrieve all tasks

--- src/domain/use-cases/tasks/get-all-tasks.service.ts
+++ src/domain/use-cases/tasks/get-all-tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { BaseUseCase } from '../base-use-case';
+import { UsersRepositoryService } from 'src/infrastructure/database/repositories/users.repository.service';
+import { TasksRepositoryService } from 'src/infrastructure/database/repositories/tasks.repository.service';
+import { ITask } from 'src/domain/interfaces/task.interface';
 
 @Injectable()
-export class GetAllTasksService {}
+export class GetAllTasksService implements BaseUseCase {
+  constructor(
+    private readonly usersRepository: UsersRepositoryService,
+    private readonly tasksRepository: TasksRepositoryService,
+  ) {}
+
+  async execute(payload: { userId: number }): Promise<ITask[]> {
+    // fetch user data
+    const userData = await this.usersRepository.findById(payload.userId);
+
+    if (!userData) {
+      throw new Error('Usuário não encontrado');
+    }
+
+    const tasks = await this.tasksRepository.findAll(payload.userId);
+
+    if (!tasks) {
+      throw new Error('Erro ao listar tarefas');
+    }
+
+    return tasks;
+  }
+}
