5031c7dc7883d3644230a1bb351be39ea9bdcf88
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-08-26 23:27:09 -0300

    Step 6 of assignment instructions: checking and fixing module dependencies

--- src/domain/use-cases/projects/projects.module.ts
+++ src/domain/use-cases/projects/projects.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { GetAllProjectsService } from './get-all-projects.service';
 import { CreateProjectService } from './create-project.service';
 import { GetProjectByIdService } from './get-project-by-id.service';
+import { DatabaseModule } from 'src/infrastructure/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [
     GetAllProjectsService,
     GetProjectByIdService,
     CreateProjectService,
   ],
+  exports: [GetAllProjectsService, GetProjectByIdService, CreateProjectService],
 })
 export class ProjectsModule {}
