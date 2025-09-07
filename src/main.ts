8fc4f67404ef1740cba62d0b1c64caeca3d238db
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-09-07 13:55:13 -0300

    Initial configuration of logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      json: false,
+      prefix: 'MyApp',
+      logLevels: ['error', 'warn', 'log', 'fatal', 'verbose', 'debug'],
+    }),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
