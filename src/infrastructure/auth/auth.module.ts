2f09bbc612b5ab9c8692ed42e75825c16638def0
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-09-14 22:52:07 -0300

    Reduced expiration time from 60 minutes to 60 seconds to test token expiration locally

--- src/infrastructure/auth/auth.module.ts
+++ src/infrastructure/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { jwtConstants } from './constants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60m' },
+      signOptions: { expiresIn: '1m' },
     }),
   ],
   providers: [AuthService],
