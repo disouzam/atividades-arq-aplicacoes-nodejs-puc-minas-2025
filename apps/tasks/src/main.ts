a3ff39e08db895a76a9ceb580036eff8f2dcd24e
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-09-21 17:10:26 -0300

    Configured console logger for Tasks microservice

--- apps/tasks/src/main.ts
+++ apps/tasks/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
+import { ConsoleLogger } from '@nestjs/common';
 import { TasksModule } from './tasks.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TasksModule);
+  const app = await NestFactory.create(TasksModule, {
+    logger: new ConsoleLogger({
+      json: false,
+      prefix: 'TaskMicroservice',
+      logLevels: ['error', 'warn', 'log', 'fatal', 'verbose', 'debug'],
+    }),
+  });
+
   app.connectMicroservice(
     {
       transport: Transport.REDIS,
