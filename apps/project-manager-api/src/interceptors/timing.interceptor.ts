bc7b59aa6e1a55f89d29b2738021f06c68cefd0e
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-09-21 15:28:43 -0300

    Deactivated logger and timing interceptor to produce a clean console log for screenshots of assignment 4

--- apps/project-manager-api/src/interceptors/timing.interceptor.ts
+++ apps/project-manager-api/src/interceptors/timing.interceptor.ts
@@ -11,11 +11,10 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class TimingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+    // console.log('Before...');
 
     const now = Date.now();
-    return next
-      .handle()
-      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+    return next.handle();
+    // .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
   }
 }
